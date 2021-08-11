1818d63f3b3854493671eda4662f7f50437c4cb5
HARSHIT.CHOUHAN <v-harshit.chouhan@kfintech.com>
2021-08-11 08:44:47 +0530

    VideoPlayer Options and Notification compt added

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,24 @@
 import "./App.css";
+import { Typography, AppBar } from "@material-ui/core";
+
+import VideoPlayer from "./components/VideoPlayer";
+import Notifications from "./components/Notifications";
+import Options from "./components/Options";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div>
+      <AppBar position="static" color="inherit">
+        <Typography variant="h4" align="center">
+          Video Chat
+        </Typography>
+      </AppBar>
+      <VideoPlayer></VideoPlayer>
+      <Options>
+        <Notifications></Notifications>
+      </Options>
+    </div>
+  );
 }
 
 export default App;

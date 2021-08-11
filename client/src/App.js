5f7f0cc0f73f4d1d2b8bb0d961f4b0901c75efc2
HARSHIT.CHOUHAN <v-harshit.chouhan@kfintech.com>
2021-08-11 14:17:56 +0530

    socketContext added in client side

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,41 @@
 import "./App.css";
 import { Typography, AppBar } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
 
 import VideoPlayer from "./components/VideoPlayer";
 import Notifications from "./components/Notifications";
 import Options from "./components/Options";
 
-function App() {
+const useStyles = makeStyles((theme) => ({
+  appBar: {
+    borderRadius: 15,
+    margin: "30px 100px",
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    width: "600px",
+    border: "2px solid black",
+
+    [theme.breakpoints.down("xs")]: {
+      width: "90%",
+    },
+  },
+
+  wrapper: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    width: "100%",
+  },
+}));
+
+const App = () => {
+  const classes = useStyles();
+
   return (
-    <div>
-      <AppBar position="static" color="inherit">
+    <div className={classes.wrapper}>
+      <AppBar className={classes.appBar} position="static" color="inherit">
         <Typography variant="h4" align="center">
           Video Chat
         </Typography>
@@ -19,6 +46,6 @@ function App() {
       </Options>
     </div>
   );
-}
+};
 
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
+
 import "./index.css";
 import App from "./App";
+import { ContextProvider } from "./SocketContext";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ContextProvider>
+      <App />
+    </ContextProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
-
-
